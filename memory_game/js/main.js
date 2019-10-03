28d7fe9d54c6f1be92be6da511c98f54d6d9b0a2
Shaw Kitajima <shawkitajima@gmail.com>
2019-10-02 19:43:14 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -36,12 +36,15 @@ function checkForMatch() {
 	}	
 };
 
-function flipCard(cardId) {
+function flipCard() {
+	var cardId = this.getAttribute('data-id');
 	cardsInPlay.push(cards[cardId].rank);
 
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
+
+	this.setAttribute("src", cards[cardId].cardImage);
 
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
@@ -52,5 +55,14 @@ function flipCard(cardId) {
 
 };
 
-flipCard(0);
-flipCard(2);
+function createBoard() {
+	for (let i = 0; i < 4; i++) {
+	var cardElement = document.createElement('img');
+	cardElement.setAttribute("src", "images/back.png")
+	cardElement.setAttribute("data-id", i);
+	cardElement.addEventListener("click", flipCard);
+	document.getElementById("game-board").appendChild(cardElement);
+}
+};
+
+createBoard();
